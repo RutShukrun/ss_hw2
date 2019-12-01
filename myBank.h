23f4b21f5b79fb9d9feba5a5ac7bb27cf696144f
RutShukrun <57772853+RutShukrun@users.noreply.github.com>
2019-12-01 19:11:11 +0200

    Update myBank.h

--- myBank.h
+++ myBank.h
@@ -1,1 +1,15 @@
+struct account
+{
+    int accNum;
+    double balance;
+    int status;
+};
+
+void open(struct account[50], double amount, int number);
+void close(struct account[50], int number);
+int status(struct account[50], int number);
+double getAmount(struct account[50], int number);
+void deposit(struct account[50], int number, double deposit);
+void withdraw(struct account[50], int number, double withdraw);
+void rate(struct account[50], int number, int rate);
 

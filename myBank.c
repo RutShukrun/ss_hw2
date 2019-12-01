4d6220c10d37dc584b4ed9fa950abf5610a4d4fe
RutShukrun <57772853+RutShukrun@users.noreply.github.com>
2019-12-01 19:10:23 +0200

    Update myBank.c

--- myBank.c
+++ myBank.c
@@ -1,1 +1,54 @@
+struct account
+{
+    int accNum;
+    double balance;
+    int status;
+};
+
+void open(struct account data[50], double amount, int number)
+{
+    data[number].status=1;
+    data[number].balance=amount;
+}
+
+void close(struct account data[50], int number)
+{
+    data[number].status=0;
+    data[number].balance=0;
+}
+
+int status(struct account data[50], int number)
+{
+    if (data[number].status==0)
+    {
+       // printf("The status of account %d is 0=close\n", number);
+        return 0; //close
+    }
+    else
+    {
+       // printf("The status of account %d is 1=open\n", number);
+        return 1; //open
+    }
+}
+
+double getAmount(struct account data[50], int number)
+{
+    return data[number].balance;
+}
+
+void deposit(struct account data[50], int number, double deposit)
+{
+    data[number].balance+=deposit;
+}
+
+void withdraw(struct account data[50], int number, double withdraw)
+{
+    data[number].balance-=withdraw;
+}
+
+void rate(struct account data[50], int number, int rate)
+{
+    double add=(data[number].balance)/100*rate;
+    data[number].balance+=add;
+}
 
